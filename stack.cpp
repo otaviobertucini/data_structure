a1636d368ab976fe4e88628af55ee90c4c1b0675
Otávio Bertucini <otaviobertucini@gmail.com>
2018-10-04 08:21:33 -0300

    Some changes made in stack

--- stack.cpp
+++ stack.cpp
@@ -1,4 +1,5 @@
 #define N 10
+#include <stddef.h>
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -32,8 +33,14 @@ int size(stack *stack){
 }
 
 void list(stack *stack){
-    for(int i = 0; i < size(stack); i++)
+    for(int i = size(stack)-1; i >= 0; i--)
         cout << stack->array[i] << endl;
+}
+
+int pop(stack *stack){
+    int x = stack->array[stack->last-1];
+    stack->last = stack->last - 1;
+    return x;
 }
 
 int main(){
@@ -43,6 +50,7 @@ int main(){
     add(&s, 2);
     add(&s, 3);
     add(&s, 4);
-
+    pop(&s);
+    pop(&s);
     list(&s);
 }
